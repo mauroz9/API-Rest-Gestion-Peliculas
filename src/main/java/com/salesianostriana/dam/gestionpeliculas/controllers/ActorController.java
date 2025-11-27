9b5ac61710b1d47f05145a7c40a049de1d1d859c
mauroz9 <mauro.serranoo26@gmail.com>
2025-11-27 14:26:53 +0100

    Documentación del ActorController

--- src/main/java/com/salesianostriana/dam/gestionpeliculas/controllers/ActorController.java
+++ src/main/java/com/salesianostriana/dam/gestionpeliculas/controllers/ActorController.java
@@ -3,6 +3,14 @@ package com.salesianostriana.dam.gestionpeliculas.controllers;
 import com.salesianostriana.dam.gestionpeliculas.dto.ActorRequestDto;
 import com.salesianostriana.dam.gestionpeliculas.dto.ActorResponseDto;
 import com.salesianostriana.dam.gestionpeliculas.services.ActorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,31 +21,115 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/actores")
 @RequiredArgsConstructor
+@Tag(name = "Actor", description = "Controlador de actores, destinado a realizar opearaciones CRUD")
 public class ActorController {
 
     private final ActorService actorService;
 
     @GetMapping
+    @Operation(summary = "Obtiene todos los actores")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Se han encontrado Actores",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ActorResponseDto.class)),
+                            examples = @ExampleObject(
+                                    value = "[{'id':1, 'nombre':Brad Pitt, 'peliculas': }]"
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se ha encontrado ningún actor"
+            )
+    })
     public ResponseEntity<List<ActorResponseDto>> findAll(){
         return ResponseEntity.ok(actorService.findAll());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obtiene un actor indicando su ID")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Se ha encontrado el Actor",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ActorResponseDto.class)),
+                            examples = @ExampleObject(
+                                    value = ""
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "No se ha encontrado el Actor"
+            )
+    })
     public ResponseEntity<ActorResponseDto> findById(@PathVariable Long id){
         return ResponseEntity.ok(actorService.findById(id));
     }
 
     @PostMapping
+    @Operation(summary = "Crea un Actor")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Actor creado",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ActorResponseDto.class)),
+                            examples =  @ExampleObject(value = "'id':1, 'nombre':'Cillian Murphy', 'peliculas':[]")
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Request, datos erroneos"
+            )
+    })
     public ResponseEntity<ActorResponseDto> create(@RequestBody ActorRequestDto dto){
         return ResponseEntity.status(HttpStatus.CREATED).body(actorService.save(dto));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Modificar un actor")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Actor modificado",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ActorResponseDto.class)),
+                            examples = @ExampleObject(value = "'id':1, 'nombre':'Mauro Murphy', 'peliculas':[]")
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Actor no encontrado"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad Request, datos erroneos"
+            )
+    })
     public ResponseEntity<ActorResponseDto> modify(@PathVariable Long id, @RequestBody ActorRequestDto dto){
         return ResponseEntity.ok(actorService.modify(id, dto));
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar un Actor")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Actor borrado"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Actor no encontrado"
+            )
+    })
     public ResponseEntity<?> delete(@PathVariable Long id){
         actorService.delete(id);
         return ResponseEntity.noContent().build();
